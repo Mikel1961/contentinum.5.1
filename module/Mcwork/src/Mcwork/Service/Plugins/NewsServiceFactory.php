db9025dec9272bdfef006abdfa97df6163a41e37
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2015-03-25 17:43:52 +0100

    Mcwork Services

---
+++ module/Mcwork/src/Mcwork/Service/Plugins/NewsServiceFactory.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * contentinum - accessibility websites
+ *
+ * LICENSE
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category contentinum
+ * @package Service
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ * @copyright Copyright (c) 2009-2013 jochum-mediaservices, Katja Jochum (http://www.jochum-mediaservices.de)
+ * @license http://www.opensource.org/licenses/bsd-license
+ * @since contentinum version 5.0
+ * @link      https://github.com/Mikel1961/contentinum-components
+ * @version   1.0.0
+ */
+namespace Mcwork\Service\Plugins;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Config\Config;
+
+/**
+ * List of plugins that are available
+ * 
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ */
+class NewsServiceFactory implements FactoryInterface
+{
+
+    /**
+     * Cache key plugins service
+     * 
+     * @var string
+     */
+    const CONTENTINUM_CFG_FILE = 'mcwork_news_plugins';
+
+    /**
+     * Name cache factory
+     * 
+     * @var string
+     */
+    const CONTENTINUM_CACHE = 'Mcwork\Cache\Data';
+    
+    
+    /**
+     *
+     * @see \Zend\ServiceManager\FactoryInterface::createService()
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('Contentinum\Configure');
+        return $this->getPluginConfig($config['default_plugins'], self::CONTENTINUM_CFG_FILE, $serviceLocator);
+    }    
+    
+    /**
+     * Get result from cache or read from php file
+     *
+     * @param string $file path to file and filename
+     * @param string $key template file ident
+     * @param ServiceLocatorInterface $sl
+     */
+    protected function getPluginConfig($plugins, $key, $sl)
+    {
+        $cache = $sl->get( self::CONTENTINUM_CACHE  );
+        if (! ($result = $cache->getItem($key))) {
+            $result = new Config($plugins);
+            $cache->setItem($key, $result);
+        }
+        return $result;
+    }    
+}
