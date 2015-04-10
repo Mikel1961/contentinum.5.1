9cea0928769d64f7404031026644c78a62b5285a
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2015-04-10 09:23:16 +0200

    Extend form

--- module/Mcwork/src/Mcwork/Form/ContactGroup.php
+++ module/Mcwork/src/Mcwork/Form/ContactGroup.php
@@ -49,6 +49,7 @@ class ContactGroup extends AbstractForms
         $html .= '<dd><a href="#fieldsetContact">' . $translation->translate('Contact data') . '</a></dd>';// tab2
         $html .= '<dd><a href="#fieldsetDescription">' . $translation->translate('Description') . '</a></dd>';
         $html .= '<dd><a href="#fieldsetAddress">' . $translation->translate('Adresse') . '</a></dd>';// tab3
+        $html .= '<dd><a href="#fieldsetEnable">Parameter</a></dd>';// tab4
         $html .= '</dl><div class="tabs-content">';// finish and start tab content area
         return $html;
     }    
@@ -339,6 +340,7 @@ class ContactGroup extends AbstractForms
             
                     'options' => array(
                         'label' => 'City',
+                        'label_attributes' => array('for' => 'contactCity'),
                         'deco-row' => $this->getDecorators(self::DECO_ELM_ROW),
                         'fieldset' => array(
                             'legend' => 'Adresse überschreiben',
@@ -352,6 +354,415 @@ class ContactGroup extends AbstractForms
                     'type' => 'Text',
                     'attributes' => array(
                         'id' => 'contactCity'
+                    )
+                )
+            ), 
+
+            array(
+                'spec' => array(
+                    'name' => 'enableTitle',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Titel anzeigen',
+                        'label_attributes' => array('for' => 'enableTitle'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableTitle',
+                        'value' => 1
+                    )
+                )
+            ), 
+            
+            
+            array(
+                'spec' => array(
+                    'name' => 'enableSalutation',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Anrede anzeigen',
+                        'label_attributes' => array('for' => 'enableSalutation'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableSalutation',
+                        'value' => 1
+                    )
+                )
+            ),
+
+            
+            array(
+                'spec' => array(
+                    'name' => 'enableFirstName',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Vorname anzeigen ',
+                        'label_attributes' => array('for' => 'enableFirstName'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableFirstName',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            array(
+                'spec' => array(
+                    'name' => 'enableLastName',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Familienname anzeigen ',
+                        'label_attributes' => array('for' => 'enableLastName'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableLastName',
+                        'value' => 1
+                    )
+                )
+            ),
+
+            array(
+                'spec' => array(
+                    'name' => 'enableBusinessTitle',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Position anzeigen ',
+                        'label_attributes' => array('for' => 'enableBusinessTitle'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableBusinessTitle',
+                        'value' => 1
+                    )
+                )
+            ),       
+
+            
+            array(
+                'spec' => array(
+                    'name' => 'enablePhoneHome',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Telefon privat anzeigen ',
+                        'label_attributes' => array('for' => 'enablePhoneHome'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enablePhoneHome',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            array(
+                'spec' => array(
+                    'name' => 'enablePhoneMobile',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Telefon mobil anzeigen ',
+                        'label_attributes' => array('for' => 'enablePhoneMobile'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enablePhoneMobile',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            
+            array(
+                'spec' => array(
+                    'name' => 'enablePhoneWork',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Telefon arbeit anzeigen ',
+                        'label_attributes' => array('for' => 'enablePhoneWork'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enablePhoneWork',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            array(
+                'spec' => array(
+                    'name' => 'enablePhoneOther',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Weitere Telefonnummer anzeigen ',
+                        'label_attributes' => array('for' => 'enablePhoneOther'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enablePhoneOther',
+                        'value' => 1
+                    )
+                )
+            ),
+
+            array(
+                'spec' => array(
+                    'name' => 'enablePhoneFax',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Fax anzeigen ',
+                        'label_attributes' => array('for' => 'enablePhoneFax'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enablePhoneFax',
+                        'value' => 1
+                    )
+                )
+            ),  
+
+            array(
+                'spec' => array(
+                    'name' => 'enableContactEmail',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'E-Mail anzeigen ',
+                        'label_attributes' => array('for' => 'enableContactEmail'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableContactEmail',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            array(
+                'spec' => array(
+                    'name' => 'enableInternet',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Internet anzeigen ',
+                        'label_attributes' => array('for' => 'enableInternet'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableInternet',
+                        'value' => 1
+                    )
+                )
+            ),            
+            
+            array(
+                'spec' => array(
+                    'name' => 'enableAlternateEmail',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Alternative E-Mailadresse anzeigen ',
+                        'label_attributes' => array('for' => 'enableAlternateEmail'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableAlternateEmail',
+                        'value' => 1
+                    )
+                )
+            ),
+
+            array(
+                'spec' => array(
+                    'name' => 'enableChat',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Chat anzeigen ',
+                        'label_attributes' => array('for' => 'enableChat'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableChat',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            array(
+                'spec' => array(
+                    'name' => 'enableFacebook',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Facebook anzeigen ',
+                        'label_attributes' => array('for' => 'enableFacebook'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableFacebook',
+                        'value' => 1
+                    )
+                )
+            ),
+
+            array(
+                'spec' => array(
+                    'name' => 'enableTwitter',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Twitter anzeigen ',
+                        'label_attributes' => array('for' => 'enableTwitter'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableTwitter',
+                        'value' => 1
+                    )
+                )
+            ),   
+
+            array(
+                'spec' => array(
+                    'name' => 'enableInstagram',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Instagram anzeigen ',
+                        'label_attributes' => array('for' => 'enableInstagram'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableInstagram',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            array(
+                'spec' => array(
+                    'name' => 'enablePinterest',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Pinterest anzeigen ',
+                        'label_attributes' => array('for' => 'enablePinterest'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enablePinterest',
+                        'value' => 1
+                    )
+                )
+            ),   
+
+            array(
+                'spec' => array(
+                    'name' => 'enableImage',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Bild anzeigen ',
+                        'label_attributes' => array('for' => 'enableImage'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableImage',
+                        'value' => 1
+                    )
+                )
+            ), 
+
+            
+            array(
+                'spec' => array(
+                    'name' => 'enableAddress',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Adresse anzeigen ',
+                        'label_attributes' => array('for' => 'enableAddress'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableAddress',
+                        'value' => 1
+                    )
+                )
+            ),  
+
+            array(
+                'spec' => array(
+                    'name' => 'enableBirthdate',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Geburtstag anzeigen ',
+                        'label_attributes' => array('for' => 'enableBirthdate'),
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableBirthdate',
+                        'value' => 1
+                    )
+                )
+            ),            
+            
+
+            array(
+                'spec' => array(
+                    'name' => 'enableDescription',
+                    'required' => false,
+            
+                    'options' => array(
+                        'label' => 'Beschreibung anzeigen',
+                        'label_attributes' => array('for' => 'enableDescription'),
+                        'fieldset' => array(
+                            'legend' => 'Parameter ein- oder ausblenden',
+                            'attributes' => array(
+                                'class' => 'content',
+                                'id' => 'fieldsetEnable'// tab1
+                            )
+                        )
+                         
+                    ),
+                    'type' => 'Checkbox',
+                    'attributes' => array(
+                        'id' => 'enableDescription',
+                        'value' => 1
                     )
                 )
             ),            
